8c44d5bb596e3cf8eaf2f4d982c66ca1853ab612
miracle2138 <454684579@qq.com>
2019-05-19 17:37:41 +0800

    test

--- src/main/java/com/liyao/App.java
+++ src/main/java/com/liyao/App.java
@@ -3,7 +3,7 @@ package com.liyao;
 import org.apache.commons.lang3.StringUtils;
 import org.omg.CORBA.INTERNAL;
 
-import java.util.function.Function;
+import java.util.function.*;
 
 /**
  * Hello world!
@@ -19,5 +19,18 @@ public class App
 
         int i = 0;
         Function<Integer, Integer> function = (a)->a+i+1+1+1;
+
+        BiFunction t = (a,b)->a.equals(b);
+        Consumer t1 = (a) -> System.out.println(a);
+        Supplier t2;
+        Predicate t3;
+
+
+        new App().tryParse((s)->Integer.parseInt(s), "999");
+
+    }
+
+    public Integer tryParse(Function<String, Integer> function, String i) {
+        return function.apply(i);
     }
 }
