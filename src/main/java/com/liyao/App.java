13f0de2f18b0bcbc1d2ddcde280f58c13ee2bbe1
miracle2138 <454684579@qq.com>
2019-05-19 19:06:44 +0800

    u

--- src/main/java/com/liyao/App.java
+++ src/main/java/com/liyao/App.java
@@ -26,7 +26,7 @@ public class App
         Predicate t3;
 
 
-        new App().tryParse((s)->Integer.parseInt(s), "9999");
+        new App().tryParse((s)->Integer.parseInt(s), "99999");
 
     }
 
